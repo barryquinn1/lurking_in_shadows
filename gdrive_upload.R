630467ffea6c06a0d5cc25d43c1c2caf16c10839
quinfer <b.quinn@qub.ac.uk>
2022-03-31 13:26:59 +0100

    lots of revisions

---
+++ gdrive_upload.R
@@ -0,0 +1,6 @@
+library(googledrive)
+drive_about()
+drive_find(n_max=30)
+drive_upload(media = "lurking_co2_RR_word.docx",
+             path = "Research/Working Papers/Energy Economics/EE_R&R",
+             type = "document",overwrite = TRUE)
