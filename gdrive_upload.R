3b3f47549f32b685280c374c621bb6c0a18a1d60
quinfer <b.quinn@qub.ac.uk>
2022-04-15 16:25:20 +0100

    new quarto version

--- gdrive_upload.R
+++ gdrive_upload.R
@@ -4,3 +4,5 @@ drive_find(n_max=30)
 drive_upload(media = "lurking_co2_RR_word.docx",
              path = "Research/Working Papers/Energy Economics/EE_R&R",
              type = "document",overwrite = TRUE)
+
+drive_download(path = "Research/Working Papers/Energy Economics/EE_R&R.docx")
